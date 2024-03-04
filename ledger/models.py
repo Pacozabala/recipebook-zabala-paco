44ec3e7f9cec58cedcc614b9312f99ac6ddea8e7
Pacozabala <paco.zabala@student.ateneo.edu>
2024-03-04 13:03:17 +0800

    put classes in models

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ingredient', args=[str(self.name)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.name)])
+    
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE
+        related_name = 'ingredients'
+    )
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE
+        reldated_name = 'recipe'
+    )
