c3dd3a5e67669a1146ecd7bca847fe21e5280b18
Pacozabala <paco.zabala@student.ateneo.edu>
2024-03-14 20:00:37 +0800

    adding admin for ledger

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Recipe, RecipeIngredient
+
+class RecipeIngredientAdmin(admin.TabularInline):
+    model = RecipeIngredient
+    
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientAdmin,]
+    
+admin.site.register(Recipe, RecipeAdmin)
+
 # Register your models here.
